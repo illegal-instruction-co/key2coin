13b876222f8b21ecb573ee05bac691bb84fb99f9
illegal-instuction-co <79029454+illegal-instuction-co@users.noreply.github.com>
2021-02-23 00:24:35 +0300

    Basis
    Create some configration basis

---
+++ server-side/api/config.js
@@ -0,0 +1,25 @@
+const Config = {}
+
+Config.branding = {
+  /*
+    Project name:
+  */
+  project_name: "K2C"
+}
+
+Config.development = {
+  /*
+    Development port
+  */
+  port: 3000
+}
+
+Config.basis = {
+  /*
+    Terminal logs for
+    error debugging
+  */
+  terminal_logs: true
+}
+
+module.exports = Config;
--- server-side/api/server.js
+++ server-side/api/server.js
@@ -1,7 +1,14 @@
 const app = require('./app');
 const http = require('http');
-const port = process.env.PORT === undefined ? 3030 : process.env.PORT
+const config = require("./config")
+const port = process.env.PORT === undefined ? config.development.port : process.env.PORT
+
+/*
+  Modular functions
+*/
+const Log = require('./functions/log')
 
 http.createServer(app).listen(port);
 
-console.log("[LOG]", "API started on " + port);
+// Create start log
+Log("[LOG]", `${config.branding.project_name} rest API started on TCP port: ` + port)
