78d382a2b8c9a177a194092bd587b37e81f6616f
illegal-instuction-co <79029454+illegal-instuction-co@users.noreply.github.com>
2021-03-10 00:19:39 +0300

    give me correct logs without confused

--- server-side/functions/log.js
+++ server-side/functions/log.js
@@ -5,7 +5,7 @@ const config = require('../config')
 */
 const Log = function(...params) {
   if(config.basis.terminal_logs) {
-    console.log(...params)
+    console.log(config.branding.project_name, ...params)
   }
 }
 
